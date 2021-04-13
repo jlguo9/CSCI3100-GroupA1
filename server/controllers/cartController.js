c2f125f5d28478409fb29b4847e52f225abd30af
jlguo9 <13zgjl@sina.cn>
2021-04-13 19:58:32 +0800

    record and cart(token not used in them for now)

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,13 +1,33 @@
 const Cart = require('../models/cart');
 
 exports.cart_add = (req,res) =>{
-
+    const cart = new Cart(req.body);
+    cart.save()
+        .then( () => {
+            res.json({State: "success","Data":""});
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 
 exports.cart_get = (req,res) =>{
-
+    Cart.find()
+        .then(Data =>{
+            res.json({State: "success", Data:""});
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
 
 exports.cart_delete = (req,res) =>{
-
+    const id = req.params.id;
+    Cart.findByIdAndDelete(id)
+        .then( () => {
+            res.json({State: "success","Data":""});
+        })
+        .catch(err =>{
+            console.log(err);
+        });
 }
