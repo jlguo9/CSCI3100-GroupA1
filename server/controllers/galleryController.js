67aec11e28898509e99bdb30cbb2707a2f3034d7
jlguo9 <13zgjl@sina.cn>
2021-04-14 19:34:47 +0800

    in menu: add authorization check

--- server/controllers/galleryController.js
+++ server/controllers/galleryController.js
@@ -1,5 +1,33 @@
 const Gallery = require('../models/gallery');
+const multer = require('multer');
+const path = require('path');
+//set storage engine
+const storage = multer.diskStorage({
+    destination: './static/uploads',
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() +
+        path.extname(file.originalname));
+    }
+})
+//Init upload
+const upload = multer({
+    storage: storage,
+    limits: {
+        filesize: 1024 * 2014 * 100
+    },
+    fileFilter: function(req,file,cb){
+
+    }
+}).single('dishImage');
 
 exports.image_post = (req,res) => {
+    upload(req,res,(err) => {
+        if(err){
+            res.status(500).json({State: "upload_failed",Data:""});
+        }
+        else{
+            console.log(req.file);
 
+        }
+    });
 }
--- server/controllers/menuController.js
+++ server/controllers/menuController.js
@@ -1,14 +1,19 @@
 const Menu = require('../models/menu');
 
 exports.menu_add = (req, res) => {
-    const menu = new Menu(req.body);
-    menu.save()
-        .then( () => {
-            res.status(201).json({"State": "success","Data":""});
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    if(req.userData.type !== "manager"){
+        res.status(403).json({State: "access_denied", Data: ""});
+    }
+    else{
+        const menu = new Menu(req.body);
+        menu.save()
+            .then( () => {
+                res.status(201).json({"State": "success","Data":""});
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 }
 exports.menu_get = (req, res) => {
     Menu.find()
@@ -20,22 +25,32 @@ exports.menu_get = (req, res) => {
         });
 }
 exports.menu_delete = (req, res) => {
-    const id = req.params.id;
-    Menu.findByIdAndDelete(id)
-        .then( () => {
-            res.status(200).json({"State": "success","Data":""});
-        })
-        .catch(err =>{
-            console.log(err);
-        });
+    if(req.userData.type !== "manager"){
+        res.status(403).json({State: "access_denied", Data: ""});
+    }
+    else{
+        const id = req.params.id;
+        Menu.findByIdAndDelete(id)
+            .then( () => {
+                res.status(200).json({"State": "success","Data":""});
+            })
+            .catch(err =>{
+                console.log(err);
+            });
+    }
 }
 exports.menu_update = (req, res) => {
-    const  id = req.params.id;
-    Menu.findByIdAndUpdate(id, req.body)
-        .then( () => {
-            res.status(200).json({"State": "success","Data":""});
-        })
-        .catch(err =>{
-            console.log(err);
-        });
+    if(req.userData.type !== "manager"){
+        res.status(403).json({State: "access_denied", Data: ""});
+    }
+    else{
+        const  id = req.params.id;
+        Menu.findByIdAndUpdate(id, req.body)
+            .then( () => {
+                res.status(200).json({"State": "success","Data":""});
+            })
+            .catch(err =>{
+                console.log(err);
+            });
+    }
 }
